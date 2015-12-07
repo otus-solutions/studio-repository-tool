1b9547d65ba14dbe16c358aa0a4de89603da45de
Diego Santos <santos.diegoferreira@gmail.com>
2015-12-07 15:16:43 -0200

    Ingore em testes a serem refatorados.

--- studio-repository-tool/src/test/java/br/org/studio/tool/base/repository/RepositoryConfigurationSuiteTest.java
+++ studio-repository-tool/src/test/java/br/org/studio/tool/base/repository/RepositoryConfigurationSuiteTest.java
@@ -1,5 +1,6 @@
 package br.org.studio.tool.base.repository;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -7,6 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
 import br.org.studio.tool.mongodb.repository.MongoRepositoryConfigurationTest;
 import br.org.studio.tool.postgres.repository.PostgresRepositoryConfigurationTest;
 
+@Ignore
 @RunWith(Suite.class)
 @SuiteClasses({ PostgresRepositoryConfigurationTest.class, MongoRepositoryConfigurationTest.class })
 public class RepositoryConfigurationSuiteTest {
