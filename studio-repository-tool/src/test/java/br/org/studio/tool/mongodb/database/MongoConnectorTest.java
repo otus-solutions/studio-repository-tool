a54f3e5210253c4efc4d645be80e0b5a980d47d2
carolinanery <carolina.nery94@gmail.com>
2016-02-12 17:02:19 -0200

    REP-22 testes MongoConnectorTest

--- studio-repository-tool/src/test/java/br/org/studio/tool/mongodb/database/MongoConnectorTest.java
+++ studio-repository-tool/src/test/java/br/org/studio/tool/mongodb/database/MongoConnectorTest.java
@@ -2,14 +2,17 @@ package br.org.studio.tool.mongodb.database;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import static org.hamcrest.CoreMatchers.*;
 import org.mockito.Mock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -17,6 +20,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoCredential;
+import com.mongodb.MongoSocketOpenException;
 import com.mongodb.ServerAddress;
 
 @RunWith(PowerMockRunner.class)
@@ -36,6 +40,7 @@ public class MongoConnectorTest {
     private MongoClient client;
 
     private MongoCredential credential;
+    
 
     @Before
     public void setup() throws Exception {
@@ -79,21 +84,21 @@ public class MongoConnectorTest {
         assertThat(connector.getUri(), containsString(PORT));
     }
 
-    /*@Test
+   /* @Test
     public void createClient_method_should_return_an_instance_of_MongoClient_when_db_server_is_accessible() {
         MongoConnector connector = MongoConnector.getConnector(HOST, PORT);
 
         assertThat(connector.createClient(credential), instanceOf(MongoClient.class));
         
     }*/
-   
-//
-//    @Test(expected = MongoSocketOpenException.class)
-//    @SuppressWarnings("unchecked")
-//    public void createClient_method_should_return_null_when_db_server_is_not_accessible() throws Exception {
-//        whenNew(MongoClient.class).withArguments(clientUri).thenThrow(MongoSocketOpenException.class);
-//        MongoConnector connector = MongoConnector.getConnector(HOST, PORT);
-//
-//        assertThat(connector.createClient(), nullValue());
-//    }
+
+    /*@Test
+    @SuppressWarnings("unchecked")
+    public void createClient_method_should_return_null_when_db_server_is_not_accessible() throws Exception {
+    	whenNew(MongoClient.class).withArguments(clientUri).thenThrow(MongoSocketOpenException.class);
+        MongoConnector connector = MongoConnector.getConnector(HOST, PORT);
+
+        assertThat(connector.createClient(credential), nullValue());
+    }
+    */
 }
